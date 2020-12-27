13685b030e8531f5ef254f77759865349f8fde81
Gowrav Tata <54976985+GowravTata@users.noreply.github.com>
2020-12-27 13:04:48 +0530

    Add files via upload

---
+++ Rescale&Resize.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec 27 13:01:45 2020
+
+@author: Gowrav Tata
+"""
+import cv2 as cv
+import os
+os.chdir(r'E:/')
+
+'''We usually do resizing and rescaling to prevent computational strain large video needs a lot of data and use lot of processing 
+rescaling a video implies modifying its width and height to a particular height'''
+
+img = cv.imread('ca.jpg')
+cv.imshow('Cat',img)
+
+def rescaleFrame(frame,scale=0.25):
+    height = int(frame.shape[0] * scale)
+    width = int(frame.shape[1] * scale)
+    dimensions = (width,height)
+    
+    return cv.resize(frame,dimensions, interpolation = cv.INTER_AREA)
+resize_image = rescaleFrame(img)
+cv.imshow('Image',resize_image)
+capture = cv.VideoCapture('DecoyBride.mp4')
+while True:
+    isTrue,frame = capture.read()  
+    
+    frame_resized = rescaleFrame(frame)
+    cv.imshow('Video',frame)
+    # display the frame by displaying the resized frame
+    cv.imshow('Video',frame_resized)
+    if cv.waitKey(20) & 0xFF==ord('q'):
+        break
+cv.waitKey
+capture.release()
+cv.destroyAllWindows()
+
