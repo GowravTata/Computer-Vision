8cccc4c94924beb43fbe6e0f8c10c177ae5eb267
Gowrav Tata <54976985+GowravTata@users.noreply.github.com>
2021-01-05 20:21:31 +0530

    Add files via upload

---
+++ Projects/Beginner/Water Paint/Water.py
@@ -0,0 +1,49 @@
+import cv2 as cv
+import numpy as np
+img = cv.imread('kerr.jpg')
+
+'''Water Colour Effect
+Parameters are source, sigma_s varies in blurring the image higher its value higher the blur
+while sigma_r is the one used to sustain the edges, while distinct colours remain pristine
+'''
+water= cv.stylization(img, sigma_s=20, sigma_r=0.4)
+# Joining the images
+
+def stackImages(scale,imgArray):
+    rows = len(imgArray)
+    cols = len(imgArray[0])
+    rowsAvailable = isinstance(imgArray[0], list)
+    width = imgArray[0][0].shape[1]
+    height = imgArray[0][0].shape[0]
+    if rowsAvailable:
+        for x in range ( 0, rows):
+            for y in range(0, cols):
+                if imgArray[x][y].shape[:2] == imgArray[0][0].shape [:2]:
+                    imgArray[x][y] = cv.resize(imgArray[x][y], (0, 0), None, scale, scale)
+                else:
+                    imgArray[x][y] = cv.resize(imgArray[x][y], (imgArray[0][0].shape[1], imgArray[0][0].shape[0]), None, scale, scale)
+                if len(imgArray[x][y].shape) == 2: imgArray[x][y]= cv.cvtColor( imgArray[x][y], cv.COLOR_GRAY2BGR)
+        imageBlank = np.zeros((height, width, 3), np.uint8)
+        hor = [imageBlank]*rows
+        hor_con = [imageBlank]*rows
+        for x in range(0, rows):
+            hor[x] = np.hstack(imgArray[x])
+        ver = np.vstack(hor)
+    else:
+        for x in range(0, rows):
+            if imgArray[x].shape[:2] == imgArray[0].shape[:2]:
+                imgArray[x] = cv.resize(imgArray[x], (0, 0), None, scale, scale)
+            else:
+                imgArray[x] = cv.resize(imgArray[x], (imgArray[0].shape[1], imgArray[0].shape[0]), None,scale, scale)
+            if len(imgArray[x].shape) == 2: imgArray[x] = cv.cvtColor(imgArray[x], cv.COLOR_GRAY2BGR)
+        hor= np.hstack(imgArray)
+        ver = hor
+    return ver
+
+
+
+oil = cv.xphoto.oilPainting(img, 7,1)
+cv.imshow('Final',oil)
+final = stackImages(1.0,([img,water]))
+cv.imwrite('Water Paint.jpg',final)
+cv.waitKey(0)
